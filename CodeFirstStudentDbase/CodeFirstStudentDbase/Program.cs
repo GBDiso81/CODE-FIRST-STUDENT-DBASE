40c8005df5f9979aed544ca76675d2d70564abb4
GBDiso81 <Gbdiso81@gmail.com>
2025-12-18 10:04:40 +0800

    CodeFirst

---
+++ CodeFirstStudentDbase/CodeFirstStudentDbase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudentDbase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                Console.WriteLine("Enter Student Full Name: ");
+                string name = Console.ReadLine();
+                var student = new Student() { StudentName = name };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("\nStudent Successfully Added...\nPress enter to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CodeFirstStudentDbase/CodeFirstStudentDbase/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CodeFirstStudentDbase.StudentConfiguration;
+
+namespace CodeFirstStudentDbase
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext() : base("SchoolDB-CodeFirstStudent")
+        {
+            Database.SetInitializer<SchoolContext>(new SchoolDBInitializer());
+        }
+
+       
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentAddress> StudentAddresses { get; set; }
+    }
+}
